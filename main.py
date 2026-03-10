388e1ce83e30d51c75756d9663395c61d4cca9fc
Jorge162017 <lop221038@uvg.edu.gt>
2026-03-09 21:35:39 -0600

    feat: construcción del AFD desde la expresión regular con el metodo directo

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-# Esto solo es para probar que si funcione el regex
-
 from src.regex import preparar_regex
+from src.afd_directo import construir_afd_directo
 
 regex = input("Ingrese la expresión regular: ")
 
@@ -8,3 +7,31 @@ regex_preparada, alfabeto = preparar_regex(regex)
 
 print("\nRegex preparada:", regex_preparada)
 print("Alfabeto:", alfabeto)
+
+afd = construir_afd_directo(regex_preparada, alfabeto)
+
+print("\nPOSTFIJO:", afd["postfijo"])
+
+print("\nFOLLOWPOS")
+for pos in sorted(afd["followpos"]):
+    print(pos, "->", sorted(afd["followpos"][pos]))
+
+print("\nESTADOS")
+for estado in afd["estados"]:
+    nombre = afd["nombres"][estado]
+    print(nombre, "=", set(estado))
+
+print("\nESTADO INICIAL")
+print(afd["nombres"][afd["estado_inicial"]])
+
+print("\nESTADOS DE ACEPTACION")
+for estado in afd["aceptacion"]:
+    print(afd["nombres"][estado], "=", set(estado))
+
+print("\nTRANSICIONES")
+for estado, trans in afd["transiciones"].items():
+
+    nombre = afd["nombres"][estado]
+
+    for simbolo, destino in trans.items():
+        print(nombre, "--", simbolo, "-->", afd["nombres"][destino])
