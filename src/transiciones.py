83e9e5f25831c8cb706fa54d1859b84f6c40895c
JoseOrdonezB <ord231329@uvg.edu.gt>
2026-03-11 13:58:42 -0600

    indicador estado inicial y final en tabla de transiciones

--- src/transiciones.py
+++ src/transiciones.py
@@ -3,15 +3,25 @@ def imprimir_tabla_transiciones(afd, alfabeto):
     estados = afd["estados"]
     transiciones = afd["transiciones"]
     nombres = afd["nombres"]
+    inicial = afd["estado_inicial"]
+    finales = afd["aceptacion"]
 
-    print("\n=== Tabla de transición ===\n")
+    print("\nTABLA DE TRANSICIÓN\n")
 
     encabezado = "Estado\t" + "\t".join(alfabeto)
     print(encabezado)
 
     for estado in estados:
 
-        fila = nombres[estado]
+        nombre_estado = nombres[estado]
+
+        if estado == inicial:
+            nombre_estado = "->" + nombre_estado
+
+        if estado in finales:
+            nombre_estado = "*" + nombre_estado
+
+        fila = nombre_estado
 
         for simbolo in alfabeto:
 
